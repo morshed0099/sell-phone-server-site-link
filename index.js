c38ff85abe389b9c5a69b592d5782131e82a6b96
morshed0099 <mdgolammorshed0099@gmail.com>
2022-11-26 03:19:54 +0600

    category and user add query setup

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
-const express= require('express')
-const app =express()
+const express=require('express')
+const app=express();
 const port=process.env.PORT || 5000
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-app.use(express())
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors=require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+console.log(process.env.DB_USER,process.env.DB_PASS);
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1m4kiwj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- const userCollection =client.db('sellPhone').collection('users');
-async function run(){
-try{
-   
-}
-finally{
+const userCollection =client.db('sellPhoneDb').collection('users')
+const categoryCollect =client.db('sellPhoneDb').collection('category')
 
-}
-}run().catch(error=>console.error(error))
+async function run(){
+   try{
+     app.get('/users',async (req,res)=>{
+        const qurey={}
+        const result=await userCollection.find(qurey).toArray();
+        res.send(result);
+     })
+     app.get('/categories',async (req,res)=>{
+        const qurey={}
+        const result=await categoryCollect.find(qurey).toArray()
+        res.send(result);
+     })
+    app.post('/users',async (req,res)=>{
+        const user=req.body;
+        console.log(user)
+        const result=await userCollection.insertOne(user);
+        res.send(result);
+    })
+  
+   }finally{
+
+   }
+}run().catch(err=>console.error(err))
 
 app.get('/',(req,res)=>{
- res.send('server is working');
+    res.send('server run')
 })
 
-app.post('/users',async(req,res)=>{
-    const user =req.body;
-    // const id=req.params.id
-    // const up=({_id:ObjectId(id)})
-    const result=await userCollection.insertOne(user);
-    res.send(result);
-   })
-
 app.listen(port,()=>{
-    console.log('server is working 5000 ')
+    console.log('server running 5000')
 })

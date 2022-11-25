b851522ec756bdfd7daa178de5b82904a5ae19b9
morshed0099 <mdgolammorshed0099@gmail.com>
2022-11-26 00:38:04 +0600

    some error fixed

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-const express=require('express');
-const app =express();
-const port =process.env.PORT || 5000
-const cors=require('cors');
-app.use(cors());
-app.use(express());
+const express= require('express')
+const app =express()
+const port=process.env.PORT || 5000
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+app.use(express())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1m4kiwj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ const userCollection =client.db('sellPhone').collection('users');
+async function run(){
+try{
+   
+}
+finally{
+
+}
+}run().catch(error=>console.error(error))
 
 app.get('/',(req,res)=>{
-    res.send('server working');
+ res.send('server is working');
 })
 
-app.listen(port,
-    console.log('server run port 5000')
-)
+app.post('/users',async(req,res)=>{
+    const user =req.body;
+    // const id=req.params.id
+    // const up=({_id:ObjectId(id)})
+    const result=await userCollection.insertOne(user);
+    res.send(result);
+   })
+
+app.listen(port,()=>{
+    console.log('server is working 5000 ')
+})

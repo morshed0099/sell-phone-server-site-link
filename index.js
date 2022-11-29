4624417e7072912341755e7218d15cef5f43c588
morshed0099 <mdgolammorshed0099@gmail.com>
2022-11-29 23:29:41 +0600

    seller and buyer add and dellte update query work

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port=process.env.PORT || 5000
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors');
+const { query } = require('express');
 
 app.use(cors())
 app.use(express.json())
@@ -21,8 +22,24 @@ const bookingCollection =client.db('sellPhoneDb').collection('bookings')
 
 async function run(){
    try{
-     app.get('/users',async (req,res)=>{
-        const qurey={}
+     app.get('/users/seller',async (req,res)=>{
+        const qurey={userRoll:'seller'}        
+        const result=await userCollection.find(qurey).toArray();
+        res.send(result);
+     })
+    //  app.get('/users',async(req,res)=>{
+    //     const query={}
+    //     const result=await userCollection.find(query).toArray()
+    //     res.send(result)
+    //  })
+   app.get('/users',async(req,res)=>{
+    const email=req.query.email;    
+    const query={email:email}
+    const result=await userCollection.findOne(query)
+    res.send(result);
+   })
+     app.get('/users/buyer',async (req,res)=>{
+        const qurey={userRoll:'buyer'}        
         const result=await userCollection.find(qurey).toArray();
         res.send(result);
      })
@@ -49,6 +66,16 @@ async function run(){
         const query=({_id:ObjectId(id)})
         const result=await producCollection.find(query).toArray()
         res.send(result)
+     })
+     app.get('productFetchWithEmail',async(req,res)=>{
+        const email=req.query.email
+        const match=seller[0].email
+        const query={
+            match:email
+        }
+        const result=await producCollection.find(query).toArray();
+        res.send(result);
+              
      })
     app.post('/users',async (req,res)=>{ 
         const user=req.body;  
@@ -79,7 +106,46 @@ async function run(){
         const result=await bookingCollection.insertOne(booking)
         res.send(result);
     })
-  
+    app.put('/users/buyer/:id',async(req,res)=>{
+        const id=req.params.id
+        const filter={_id:ObjectId(id)}
+        const options = { upsert: true };
+        const status=req.body 
+        console.log(status)
+        const updateDoc={
+            $set:{
+                status:true
+           }  
+          }
+          const result= await userCollection.updateOne(filter,updateDoc,options)
+          res.send(result);
+    })
+    app.put('/users/seller/:id',async(req,res)=>{
+        const id=req.params.id
+        const filter={_id:ObjectId(id)}
+        const options = { upsert: true };
+        const status=req.body 
+        console.log(status)
+        const updateDoc={
+            $set:{
+                status:true
+           }  
+          }
+          const result= await userCollection.updateOne(filter,updateDoc,options)
+          res.send(result);
+    })
+    app.delete('/users/buyer/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query={_id:ObjectId(id)}
+        const result=await userCollection.deleteOne(query);
+        res.send(result);
+    })
+    app.delete('/users/seller/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query={_id:ObjectId(id)}
+        const result=await userCollection.deleteOne(query);
+        res.send(result);
+    })
    }finally{
 
    }
